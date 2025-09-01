ae68df4194fe0727344e7b078a79e57c4e3e2e21
AliDeWeb <AliMoradi0Business@gmail.com>
2025-09-01 18:25:33 +0330

    feat: enhance layout and typography in Introduction component for better visual appeal

--- src/components/slides/Introduction.component.tsx
+++ src/components/slides/Introduction.component.tsx
@@ -1,10 +1,10 @@
 export const Introduction = () => {
   return (
-    <div className="full-page bottom-section introduction-bg-img">
-      <h1 data-aos="fade-up" className="heading">
+    <div className="full-page flex items-center justify-center flex-col sm:bottom-section introduction-bg-img">
+      <h1 data-aos="fade-up" className="heading text-[80px]">
         velura
       </h1>
-      <p data-aos="fade-down" className="paragraph">
+      <p data-aos="fade-down" className="paragraph text-[20px] text-center">
         where ideas become open
       </p>
     </div>
