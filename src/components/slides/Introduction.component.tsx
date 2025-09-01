3b51b9f120a3ff643e6b4c6ecc40231f51e1beed
AliDeWeb <AliMoradi0Business@gmail.com>
2025-09-01 18:52:48 +0330

    feat: add LinkedIn and GitHub icons to Introduction component for enhanced social media presence

--- src/components/slides/Introduction.component.tsx
+++ src/components/slides/Introduction.component.tsx
@@ -1,12 +1,27 @@
+import linkedinIcon from "../../assets/icons/linkedin.png";
+import githubIcon from "../../assets/icons/github.png";
+
 export const Introduction = () => {
   return (
-    <div className="full-page h-dvh flex items-center justify-center flex-col sm:bottom-section introduction-bg-img">
+    <div className="full-page h-dvh flex items-center justify-center flex-col sm:bottom-section introduction-bg-img relative">
       <h1 data-aos="fade-up" className="heading text-[80px]">
         <a href="https://www.linkedin.com/company/velura-open-source">velura</a>
       </h1>
       <p data-aos="fade-down" className="paragraph text-[20px] text-center">
         where ideas become open
       </p>
+
+      <div className="absolute bottom-20 inset-x-0 sm:right-auto bg-white/20 rounded-xl p-2 sm:left-10 flex mx-auto justify-center w-max items-center sm:flex-col gap-5">
+        <a
+          className="size-10"
+          href="https://www.linkedin.com/company/velura-open-source"
+        >
+          <img src={linkedinIcon} alt="linkedin-icon" />
+        </a>
+        <a className="size-10" href="https://github.com/VeluraOpenSource">
+          <img src={githubIcon} alt="github-icon" />
+        </a>
+      </div>
     </div>
   );
 };
