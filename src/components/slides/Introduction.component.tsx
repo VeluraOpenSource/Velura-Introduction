c8b140aeb84598c9bd92484d70366b1198d90d61
AliDeWeb <AliMoradi0Business@gmail.com>
2025-09-01 18:44:59 +0330

    feat: wrap heading text in anchor tag for improved accessibility and navigation

--- src/components/slides/Introduction.component.tsx
+++ src/components/slides/Introduction.component.tsx
@@ -2,7 +2,7 @@ export const Introduction = () => {
   return (
     <div className="full-page h-dvh flex items-center justify-center flex-col sm:bottom-section introduction-bg-img">
       <h1 data-aos="fade-up" className="heading text-[80px]">
-        velura
+        <a href="https://www.linkedin.com/company/velura-open-source">velura</a>
       </h1>
       <p data-aos="fade-down" className="paragraph text-[20px] text-center">
         where ideas become open
