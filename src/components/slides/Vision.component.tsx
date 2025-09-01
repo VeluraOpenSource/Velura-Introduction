dc9138077ad41759d775c6567ba95544a914692b
AliDeWeb <AliMoradi0Business@gmail.com>
2025-09-01 16:56:16 +0330

    fix: fix some issues

--- src/components/slides/Vision.component.tsx
+++ src/components/slides/Vision.component.tsx
@@ -1,6 +1,6 @@
 export const Vision = () => {
   return (
-    <div className="full-page dark-bg center-section">
+    <div className="full-page center-section">
       <h1 className="heading wrap-heading">what is the vision?</h1>
       <p className="paragraph">What needs to change? Why?</p>
     </div>
