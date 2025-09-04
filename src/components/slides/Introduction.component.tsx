99e250b7dc018525afd341681d8efd12c1c21fb7
AliDeWeb <AliMoradi0Business@gmail.com>
2025-09-04 20:24:02 +0330

    feat: integrate typewriter effect in Introduction component for dynamic text display

--- src/components/slides/Introduction.component.tsx
+++ src/components/slides/Introduction.component.tsx
@@ -1,14 +1,40 @@
+import React from 'react';
+import Typewriter from 'typewriter-effect';
+
 import linkedinIcon from '../../assets/icons/linkedin.png';
 import githubIcon from '../../assets/icons/github.png';
 
 export const Introduction = () => {
+  const [startDescription, setStartDescription] = React.useState(false);
+
   return (
     <div className="full-page sm:bottom-section introduction-bg-img relative flex h-dvh flex-col items-center justify-center">
-      <h1 data-aos="fade-up" className="heading text-[80px]">
-        <a href="https://www.linkedin.com/company/velura-open-source">velura</a>
+      <h1 className="heading text-[80px]">
+        <a href="https://www.linkedin.com/company/velura-open-source">
+          <Typewriter
+            options={{ cursor: '' }}
+            onInit={(typewriter) => {
+              typewriter
+                .pauseFor(200)
+                .typeString('velura')
+                .callFunction(() => setStartDescription(true))
+                .start();
+            }}
+          />
+        </a>
       </h1>
-      <p data-aos="fade-down" className="paragraph text-center text-[20px]">
-        where ideas become open
+      <p className="paragraph text-center text-[20px]">
+        {startDescription && (
+          <Typewriter
+            options={{ cursor: '_' }}
+            onInit={(typewriter) => {
+              typewriter
+                .pauseFor(500)
+                .typeString('where ideas become open')
+                .start();
+            }}
+          />
+        )}
       </p>
 
       <div className="absolute inset-x-0 bottom-20 mx-auto flex w-max items-center justify-center gap-5 rounded-xl bg-white/20 p-2 sm:right-auto sm:left-10 sm:flex-col">
